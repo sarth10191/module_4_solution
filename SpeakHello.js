1d412434252029e59f4987dc0cc0928f6356adf2
sarth10191 <sarth.sheth21@vit.edu>
2023-09-04 15:53:12 +0530

    fitsr

---
+++ SpeakHello.js
@@ -0,0 +1,17 @@
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
+
+
+// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+(function (window) {
+    var speakWord = "Hello";
+    var helloSpeaker = {};
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
