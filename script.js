b236ce04959d4e1de0ebce0fdd4e3d7a04dbdc1c
sarth10191 <99161687+sarth10191@users.noreply.github.com>
2023-09-04 16:20:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
-            helloSpeaker.speak(names[i]);
+            byeSpeaker.speak(names[i]);
         } else {
             helloSpeaker.speak(names[i]);
         }
